3a4437d9f0581e1f5b5d5be8caeb51f97a9f5932
Rogerio-H <rogeriohanke@gmail.com>
2022-05-16 11:54:03 -0300

    PYTHONNNNN

---
+++ exercicios.py
@@ -0,0 +1,19 @@
+
+b = float(input("Digite o lado do terreno: "))
+
+h = float(input("Digite a frente do terreno: "))
+
+area = b * h
+print("A area do retângulo é: ", area)
+
+if area < 1000:
+    print("Terreno pequeno")
+
+elif area >= 1000 and area < 5000:
+    print("Sítio")
+
+elif area >= 5000 and area < 10000:
+    print("Hectare")
+
+elif area >= 10000:
+    print("Fazenda")
---
+++ testescomentados.py
@@ -0,0 +1,31 @@
+"""
+testes 
+
+hw = ('Hello World')
+
+for i in range(1,5):    
+    print(hw)
+
+"""
+"""
+Exercício 001
+
+b = float(input("Digite o lado do terreno: "))
+
+h = float(input("Digite a frente do terreno: "))
+
+area = b * h
+print("A area do retângulo é: ", area)
+
+if area < 1000:
+    print("Terreno pequeno")
+
+elif area >= 1000 and area < 5000:
+    print("Sítio")
+
+elif area >= 5000 and area < 10000:
+    print("Hectare")
+
+elif area >= 10000:
+    print("Fazenda")
+"""
