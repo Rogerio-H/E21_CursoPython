73e627ef54d7f8d1a636a34d74075f46675e771a
Rogério Hanke <rogeriohanke@gmail.com>
2022-09-04 20:17:07 -0300

    agr ta certinho

--- django/app1/forms.py
+++ django/app1/forms.py
@@ -1,10 +1,9 @@
 from django import forms
 from .models import Pessoas
 
-
-class PessoasForm(forms.Form):
-    pessoas_nome = forms.CharField(label = 'Nome ', max_length=100)
-    pessoas_idade = forms.IntegerField(label= 'Idade ')
+class PessoasForm(forms.ModelForm):
+    # pessoas_nome = forms.CharField(label = 'Nome ', max_length=100)
+    # pessoas_idade = forms.IntegerField(label= 'Idade ')
 
     class Meta:
         model = Pessoas
