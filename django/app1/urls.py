b1e1f5284b7cd9fd15fea6fd53eb038c848dfa41
Rogério Hanke <rogeriohanke@gmail.com>
2022-09-09 10:13:40 -0300

    commit atrasado

--- django/app1/urls.py
+++ django/app1/urls.py
@@ -3,8 +3,11 @@ from . import views
 
 urlpatterns = [ 
     path('', views.index),
-    path('pessoas/', views.pessoasview),
-    path('db1', views.db1),
-    path('db2', views.db2),
-    path('thanks/', views.thanks)
+    path('pessoas_list/', views.pessoas_list),
+    path('pessoa_add', views.pessoa_add),
+    path('pessoa_edit/<int:id>', views.pessoa_edit, name="edit-pessoa"),
+    path('pessoa_delete/<int:id>', views.pessoa_delete, name="delete-pessoa"),
+    # path('db1', views.db1),
+    # path('db2', views.db2),
+    # path('thanks/', views.thanks)
 ]
--- django/app1/views.py
+++ django/app1/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import PessoasForm
 from django.http import HttpResponseRedirect
 from .models import Pessoas
-
+from django.contrib import messages
 # Create your views here.
 
 @login_required
@@ -15,29 +15,69 @@ def index(request):
                     {'dados': data}
                     )
 
+# @login_required
+# def db1(request):
+
+#     data = "Versao 0.01 - Dashboard 1 (um) "
+
+#     return render(request, 'dashboard1.html',
+#                   {'dados': data}
+#                   )
+
+
+# @login_required
+# def db2(request):
+
+#     data = "Versao 0.01 - Dashboard 2 (dois)"
+
+#     return render(request, 'dashboard2.html',
+#                   {'dados': data}
+#                   )
+
+
+# @login_required
+# def pessoasview(request):
+
+#     if request.method == 'POST':
+#         form = PessoasForm(request.POST)
+#         if form.is_valid():
+#             nova_pessoa = form.save(commit=False)
+#             nova_pessoa.pessoa_nome = form.cleaned_data['pessoa_nome']
+#             nova_pessoa.pessoa_idade = form.cleaned_data['pessoa_idade']
+#             nova_pessoa.save()
+#             return HttpResponseRedirect('/pessoas/')
+#     else:
+#         form = PessoasForm()
+#     pessoas = Pessoas.objects.all()
+    
+#     return render(request, 'pessoasview.html',
+#                   {
+#                       'form': form,
+#                       'pessoas' : pessoas
+#                   }
+
+#                   )
+
+
+# @login_required
+# def thanks(request):
+#     mensagem = "Obrigado!"
+#     return render(request, 'thanks.html', {'mensagem': mensagem}
+#                   )
+
 @login_required
-def db1(request):
+def pessoas_list(request):
+    search = request.GET.get('search')
+    if search:
+        pessoas = Pessoas.objects.filter(pessoa_nome__icontains=search)
+    else:
+        pessoas = Pessoas.objects.all()
 
-    data = "Versao 0.01 - Dashboard 1 (um) "
-
-    return render(request, 'dashboard1.html',
-                  {'dados': data}
-                  )
-
-
-@login_required
-def db2(request):
-
-    data = "Versao 0.01 - Dashboard 2 (dois)"
-
-    return render(request, 'dashboard2.html',
-                  {'dados': data}
-                  )
-
+    return render(request, 'pessoas_list.html',
+                  {'pessoas': pessoas})
 
 @login_required
-def pessoasview(request):
-
+def pessoa_add(request, id=0):
     if request.method == 'POST':
         form = PessoasForm(request.POST)
         if form.is_valid():
@@ -45,29 +85,39 @@ def pessoasview(request):
             nova_pessoa.pessoa_nome = form.cleaned_data['pessoa_nome']
             nova_pessoa.pessoa_idade = form.cleaned_data['pessoa_idade']
             nova_pessoa.save()
-            return HttpResponseRedirect('/pessoas/')
+            return HttpResponseRedirect('/pessoas_list')
     else:
         form = PessoasForm()
+        
     pessoas = Pessoas.objects.all()
-    
-    return render(request, 'pessoasview.html',
-                  {
-                      'form': form,
-                      'pessoas' : pessoas
-                  }
 
-                  )
-
+    return render(request, 'pessoa_add.html',
+                    {
+                        'form': form,
+                        'pessoas': pessoas
+                    }
+                    )   
 
 @login_required
-def thanks(request):
-    mensagem = "Obrigado!"
-    return render(request, 'thanks.html', {'mensagem': mensagem}
-                  )
+def pessoa_edit(request, id):
+    pessoa = get_object_or_404(Pessoas, pk=id)
+    form = PessoasForm(instance=pessoa)
+    if ( request.method == 'POST'):
+        form = PessoasForm(request.POST, instance=pessoa)
+        if (form.is_valid()):
+            pessoa.save()
+            return redirect('/pessoas_list')
+        else:
+            return render(request, 'pessoa_edit.html', {'form': form, 'pessoa': pessoa})
+    else:
+        return render(request, 'pessoa_edit.html', {'form': form, 'pessoa': pessoa})
 
-
-
-
+@login_required
+def pessoa_delete(request, id):
+    pessoa = get_object_or_404(Pessoas, pk=id)
+    pessoa.delete()
+    messages.info(request, 'Pessoa apagado do banco de dados')
+    return redirect('/pessoas_list')
 
 
 
